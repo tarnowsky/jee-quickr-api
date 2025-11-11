62238aa30d6a5839485569d54de0d0afba9467b6
Michał Tarnowski <michal.tarnowski03@gmail.com>
2025-11-11 12:46:48 +0100

    repositories done
    Signed-off-by: Michał Tarnowski <michal.tarnowski03@gmail.com>

---
+++ src/main/java/pg/eti/kask/jee/quickr/order/repository/persistance/OrderPersistenceRepository.java
@@ -0,0 +1,51 @@
+package pg.eti.kask.jee.quickr.order.repository.persistance;
+
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.ws.rs.BadRequestException;
+import pg.eti.kask.jee.quickr.order.entity.Order;
+import pg.eti.kask.jee.quickr.order.repository.api.OrderRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class OrderPersistenceRepository implements OrderRepository {
+
+    private EntityManager em;
+
+    @PersistenceContext
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Optional<Order> findById(UUID id) {
+        return Optional.ofNullable(em.find(Order.class, id));
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return em.createQuery("select o from Order o", Order.class).getResultList();
+    }
+
+    @Override
+    public Optional<Order> create(Order entity) {
+        em.persist(entity);
+        return Optional.of(entity);
+    }
+
+    @Override
+    public Optional<Order> update(Order entity) {
+        em.merge(entity);
+        return Optional.of(entity);
+    }
+
+    @Override
+    public Optional<Order> delete(UUID id) {
+        Order entityToDelete = em.find(Order.class, id);
+        em.remove(entityToDelete);
+        return Optional.of(entityToDelete);
+    }
+}
---
+++ src/main/java/pg/eti/kask/jee/quickr/order/repository/persistance/VenuePersistenceRepository.java
@@ -0,0 +1,51 @@
+package pg.eti.kask.jee.quickr.order.repository.persistance;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import pg.eti.kask.jee.quickr.order.entity.Venue;
+import pg.eti.kask.jee.quickr.order.repository.api.VenueRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class VenuePersistenceRepository implements VenueRepository {
+
+    private EntityManager em;
+
+    @PersistenceContext
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Optional<Venue> findById(UUID id) {
+        return Optional.ofNullable(em.find(Venue.class, id));
+    }
+
+    @Override
+    public List<Venue> findAll() {
+        return em.createQuery("select v from Venue v", Venue.class).getResultList();
+    }
+
+    @Override
+    public Optional<Venue> create(Venue entity) {
+        em.persist(entity);
+        return Optional.of(entity);
+    }
+
+    @Override
+    public Optional<Venue> update(Venue entity) {
+        em.merge(entity);
+        return Optional.of(entity);
+    }
+
+    @Override
+    public Optional<Venue> delete(UUID id) {
+        Venue venueToDelete = em.find(Venue.class, id);
+        em.remove(venueToDelete);
+        return Optional.of(venueToDelete);
+    }
+
+
+}
