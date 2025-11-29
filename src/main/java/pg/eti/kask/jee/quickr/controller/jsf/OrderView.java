747e7139c84c1e868b874ab5955d84ee18c9544f
Michał Tarnowski <michal.tarnowski03@gmail.com>
2025-11-29 13:11:22 +0100

    feat: Add security context and authorization checks to OrderView init method.

--- src/main/java/pg/eti/kask/jee/quickr/controller/jsf/OrderView.java
+++ src/main/java/pg/eti/kask/jee/quickr/controller/jsf/OrderView.java
@@ -12,6 +12,7 @@ import pg.eti.kask.jee.quickr.component.ModelFunctionFactory;
 import pg.eti.kask.jee.quickr.entity.Order;
 import pg.eti.kask.jee.quickr.model.order.OrderModel;
 import pg.eti.kask.jee.quickr.service.OrderService;
+import jakarta.security.enterprise.SecurityContext;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -24,14 +25,17 @@ public class OrderView implements Serializable {
     private OrderService service;
     private final ModelFunctionFactory factory;
 
+    private final SecurityContext securityContext;
+
     @EJB
     public void setService(OrderService service) {
         this.service = service;
     }
 
     @Inject
-    public OrderView(ModelFunctionFactory factory) {
+    public OrderView(ModelFunctionFactory factory, SecurityContext securityContext) {
         this.factory = factory;
+        this.securityContext = securityContext;
     }
 
     @Setter
@@ -44,7 +48,15 @@ public class OrderView implements Serializable {
     public void init() throws IOException {
         java.util.Optional<Order> orderOptional = service.findById(id);
         if (orderOptional.isPresent()) {
-            this.order = factory.orderToModel().apply(orderOptional.get());
+            Order orderEntity = orderOptional.get();
+            if (securityContext.isCallerInRole(pg.eti.kask.jee.quickr.entity.enums.UserRoles.ADMIN) ||
+                    (orderEntity.getUser() != null && orderEntity.getUser().getLogin()
+                            .equals(securityContext.getCallerPrincipal().getName()))) {
+                this.order = factory.orderToModel().apply(orderEntity);
+            } else {
+                FacesContext.getCurrentInstance().getExternalContext().responseSendError(
+                        HttpServletResponse.SC_FORBIDDEN, "Access denied");
+            }
         } else {
             FacesContext.getCurrentInstance().getExternalContext().responseSendError(
                     HttpServletResponse.SC_NOT_FOUND, "Order not found");
